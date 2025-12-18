c0f9ea8bedf1dc0a8e88d4adccb6b2bcbd2ddba0
den <evtushokdenis2004@gmail.com>
2025-12-19 02:35:27 +0300

    commit files

--- main.py
+++ main.py
@@ -1,35 +1,44 @@
 import asyncio
-import logging
-import json
 import os
+import json
 
 from aiogram import Bot, Dispatcher, types, F
-from aiogram.utils.keyboard import ReplyKeyboardBuilder
-from aiogram.enums.content_type import ContentType
-from aiogram.filters import CommandStart
-from aiogram.enums.parse_mode import ParseMode
+from aiogram.enums import ParseMode, ContentType
+from aiogram.types import WebAppInfo, KeyboardButton, ReplyKeyboardMarkup
 
-logging.basicConfig(level=logging.INFO)
 
 bot = Bot('7246301763:AAFD_8vaxy4cUm3tOJLWrAAgtZ0ZKoqabsM')
 dp = Dispatcher()
 
-
-@dp.message(CommandStart())
-async def start(message: types.Message):
-    webAppInfo = types.WebAppInfo(url="https://miniwebapps.ru")
-    builder = ReplyKeyboardBuilder()
-    builder.add(types.KeyboardButton(text='Отправить данные', web_app=webAppInfo))
-
-    await message.answer(text='Привет!', reply_markup=builder.as_markup())
+@dp.message()
+async def show_miniapp(message: types.Message):
+    keyboard = ReplyKeyboardMarkup(
+        keyboard=[
+            [KeyboardButton(
+                text="Открыть MiniApp",
+                ##### Заменить ниже:
+                web_app=WebAppInfo(url="https://ССЫЛКА-КОТОРУЮ-ВЫ-ПОЛУЧИТЕ-ПОЗЖЕ/")
+            )]
+        ],
+        resize_keyboard=True
+    )
+    await message.answer("Нажмите кнопку ниже, чтобы открыть Mini App:", reply_markup=keyboard)
 
 
 @dp.message(F.content_type == ContentType.WEB_APP_DATA)
-async def parse_data(message: types.Message):
-    data = json.loads(message.web_app_data.data)
-    await message.answer(f'<b>{data["title"]}</b>\n\n<code>{data["desc"]}</code>\n\n{data["text"]}',
-                         parse_mode=ParseMode.HTML)
+async def handle_webapp_data(message: types.Message):
+    try:
+        data = json.loads(message.web_app_data.data)
+        name = data.get("name", "Неизвестный пользователь")
+        msg = data.get("message", "(пусто)")
 
+        await message.answer(
+            f"<b>{name}</b> отправил(а) сообщение:\n\n{msg}",
+            parse_mode=ParseMode.HTML
+        )
+
+    except Exception as e:
+        await message.answer("Произошла ошибка при разборе данных из Mini App")
 
 async def main():
     await bot.delete_webhook(drop_pending_updates=True)
