5aea51568feff807ff5b813d0c254044eb6631bb
den <evtushokdenis2004@gmail.com>
2026-03-06 17:39:22 +0300

    commit file

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
-from flask import Flask, render_template
+import uvicorn
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+import os
 
-app = Flask(__name__)
+app = FastAPI()
 
-
-@app.route("/")
-def web():
-    return render_template("index.html")
-
+@app.get("/")
+async def index():
+    # Проверяем, существует ли файл, чтобы избежать ошибки 500
+    if os.path.exists("templates/index.html"):
+        return FileResponse("templates/index.html")
+    return {"error": "Файл index.html не найден в папке проекта"}
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    # В FastAPI используется uvicorn.run вместо app.run
+    uvicorn.run(app, host="0.0.0.0", port=8000)
